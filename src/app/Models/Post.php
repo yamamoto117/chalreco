1d5260abc63faffb2178d66fb7276ebaa9a32c70
yamamoto117 <r.yamamoto117@gmail.com>
2023-09-23 08:40:18 +0900

    feat: #74 ホーム画面のデザインを改善する

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -36,5 +37,26 @@ class Post extends Model
     public function getCountGoodsAttribute(): int
     {
         return $this->goods->count();
+    }
+
+    public function getTimeAgoAttribute()
+    {
+        $postDate = $this->created_at;
+        $now = Carbon::now();
+        $diff = $postDate->diff($now);
+
+        if ($diff->y > 0) {
+            return $diff->y . "年前";
+        } elseif ($diff->m > 0) {
+            return $diff->m . "ヶ月前";
+        } elseif ($diff->d > 0) {
+            return $diff->d . "日前";
+        } elseif ($diff->h > 0) {
+            return $diff->h . "時間前";
+        } elseif ($diff->i > 0) {
+            return $diff->i . "分前";
+        } else {
+            return "たった今";
+        }
     }
 }

7bd821e48777ef3f4d0cf452e77de972382e4930
yamamoto117 <r.yamamoto117@gmail.com>
2023-10-28 18:28:35 +0900

    refactor: #129 Bladeを共通化する

--- src/resources/views/components/button.blade.php
+++ src/resources/views/components/button.blade.php
@@ -1,7 +1,3 @@
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full flex justify-center text-base leading-6 bg-orange-400 mt-5 hover:bg-orange-500 text-white py-2 px-4 rounded-full']) }}>
     {{ $slot }}
 </button>
-
-{{-- <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button> --}}
--- src/resources/views/components/input.blade.php
+++ src/resources/views/components/input.blade.php
@@ -1,7 +1,3 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
-
-{{-- @props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}> --}}

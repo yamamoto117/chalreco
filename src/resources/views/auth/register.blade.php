687dd9feabc003bde80dc53d65c6a472d70558e9
yamamoto117 <r.yamamoto117@gmail.com>
2023-11-08 21:35:10 +0900

    refactor: #159 戻るボタンのリファクタリングをする

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -3,19 +3,7 @@
         新規登録
     </x-slot>
 
-    <div class="flex justify-start">
-        <div class="pt-1 pl-1">
-            <a href="{{ route('login')}}" class=" text-2xl font-medium rounded-full text-gray-700 hover:bg-gray-100 hover:text-gray-600 float-right">
-                <svg class="m-2 h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                    <g>
-                        <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z">
-                        </path>
-                    </g>
-                </svg>
-            </a>
-        </div>
-    </div>
-
+    <x-back />
     <div class="p-4">
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
